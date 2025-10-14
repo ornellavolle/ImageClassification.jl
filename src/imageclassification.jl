caca131c52cdcbe4d1821e54c978a46cc2eeaaa1
ornellavolle <ornellavolle@gmail.com>
2025-10-14 08:20:56 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.56

---
+++ src/imageclassification.jl
@@ -0,0 +1,5 @@
+module imageclassification
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using imageclassification
+using Test
+
+@testset "imageclassification.jl" begin
+    # Write your tests here.
+end
