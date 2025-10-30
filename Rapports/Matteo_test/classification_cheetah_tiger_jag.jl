a60af4ff3c08b3a72f340d1d3a989dfab2690a9f
mschweiz38 <100361961+mschweiz38@users.noreply.github.com>
2025-10-30 21:46:15 +0100

    30/10

--- Rapports/Matteo_test/classification_cheetah_tiger_jag.jl
+++ Rapports/Matteo_test/classification_cheetah_tiger_jag.jl
@@ -62,4 +62,44 @@ val_labels = coerce(val_labels, Multiclass)
 
 
 #Visualiser une image
-train_images[2500]
+train_images[2501]
+
+import MLJFlux
+#using Flux
+#using MLUtils  # pour flatten
+#using Random
+
+# --- Définition du constructeur ---
+struct MyConvBuilder
+    filter_size::Int      # taille des filtres de convolution (ex: 3)
+    channels1::Int        # nombre de canaux dans le 1er bloc conv
+    channels2::Int
+    channels3::Int
+end
+
+function MLJFlux.build(b::MyConvBuilder, rng, image_size, n_out, n_channels)
+    # image_size = (height, width)
+    k, c1, c2, c3 = b.filter_size, b.channels1, b.channels2, b.channels3
+    mod(k, 2) == 1 || error("`filter_size` must be odd.")
+    p = div(k - 1, 2)  # padding pour garder même taille après conv
+    init = Flux.glorot_uniform(rng)
+
+    front = Chain(
+        Conv((k, k), n_channels => c1, pad=(p, p), relu, init=init),
+        MaxPool((2, 2)),
+        Conv((k, k), c1 => c2, pad=(p, p), relu, init=init),
+        MaxPool((2, 2)),
+        Conv((k, k), c2 => c3, pad=(p, p), relu, init=init),
+        MaxPool((2, 2)),
+        MLUtils.flatten
+    )
+
+    # Taille de sortie du CNN avant la couche fully connected
+    d = Flux.outputsize(front, (image_size..., n_channels, 1)) |> first
+
+    # Réseau complet
+    return Chain(front, Dense(d, n_out, init=init))
+end
+
+
+
