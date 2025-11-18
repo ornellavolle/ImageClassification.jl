08788c71c709be1db3893bdcb5ce3be0a3687b6e
ornellavolle <ornellavolle@gmail.com>
2025-11-18 08:39:16 +0100

    8h38
    essaye pour le bonito

--- Rapports/Ornella/classificationBonitoTest.jl
+++ Rapports/Ornella/classificationBonitoTest.jl
@@ -1,58 +1,23 @@
 using GLMakie
-using FileIO, Glob
-using GLMakie: Button, Slider, Label, image!
-using ImageClassification
 
+# Active GLMakie pour l'affichage
+GLMakie.activate!()
 
+# Crée une fenêtre
+fig = Figure(size = (400, 200))
 
-load_dataset()
+# Ajoute deux boutons
+btn_prev = Button(fig[1, 1], label = "Précédent")
+btn_next = Button(fig[1, 2], label = "Suivant")
 
-# --- App GLMakie ---
-App() do app
-    # Observable pour l'image courante
-    current_index = Observable(1)
-    current_img = Observable(load(images[current_index[]]))
-
-    # Figure principale
-    fig = Figure(parent = app, size = (900, 700))
-
-    # Zone image
-    ax = Axis(fig[1, 1]; title = "Classification d’images")
-    img_plot = image!(ax, current_img[])
-    hidedecorations!(ax)
-    autolimits!(ax)
-
-    # Boutons précédent / suivant
-    prev_btn = Button(fig[2, 1], label = "Précédent")
-    next_btn = Button(fig[2, 2], label = "Suivant")
-
-    # Slider
-    slider = Slider(fig[3, 1:2], range = 1:length(images), startvalue = 1)
-
-    # Label
-    label = Label(fig[4, 1:2], text = "Image 1 / $(length(images))", tellwidth = false)
-
-    # --- Fonction pour mettre à jour l'image ---
-    function update_image(index)
-        current_index[] = index
-        current_img[] = load(images[index])
-        img_plot[1][] = current_img[]
-        slider.value[] = index
-        label.text = "Image $index / $(length(images))"
-    end
-
-    # Callbacks
-    on(slider.value) do val
-        update_image(round(Int, val))
-    end
-
-    on(prev_btn.clicks) do _
-        update_image(max(1, current_index[] - 1))
-    end
-
-    on(next_btn.clicks) do _
-        update_image(min(length(images), current_index[] + 1))
-    end
-
-    display(fig)
+# Logique des boutons (affiche un message dans la console)
+on(btn_prev.clicks) do _
+    println("Bouton Précédent cliqué !")
 end
+
+on(btn_next.clicks) do _
+    println("Bouton Suivant cliqué !")
+end
+
+# Affiche la fenêtre
+display(fig)
--- Rapports/Ornella/exemple.jl
+++ Rapports/Ornella/exemple.jl
@@ -1,16 +1,51 @@
-using Bonito
-# Create a reactive counter app
-app = App() do session
-    count = Observable(0)
-    button = Button("Click me!")
-    on(click-> (count[] += 1), button)
-    return DOM.div(button, DOM.h1("Count: ", count))
+using WGLMakie, Images
+
+# Active GLMakie pour l'affichage
+GLMakie.activate!()
+
+# Fonction pour créer l'interface
+function interface_images()
+    # Crée une fenêtre Makie
+    fig = Figure(size = (800, 600))
+
+    # Zone pour afficher l'image
+    ax = GLMakie.Axis(fig[1, 1])
+    hidexdecorations!(ax)
+    hideydecorations!(ax)
+
+    # Génère des images aléatoires pour l'exemple
+    images = [rand(RGB, 400, 400) for _ in 1:5]  # 5 images aléatoires
+    current_index = Observable(1)  # Index de l'image actuelle
+
+    # Affiche l'image actuelleusin
+    image_obs = @lift(images[$current_index])
+    image!(ax, image_obs)
+
+    # Boutons "Précédent" et "Suivant"
+    btn_prev = Button(fig[2, 1], label = "Précédent")
+    btn_next = Button(fig[2, 2], label = "Suivant")
+
+    # Logique des boutons
+    on(btn_prev.clicks) do _
+        current_index[] = max(1, current_index[] - 1)
+        @info "Bouton Précédent cliqué - Index: $(current_index[])"
+    end
+
+    on(btn_next.clicks) do _
+        current_index[] = min(length(images), current_index[] + 1)
+        @info "Bouton Suivant cliqué - Index: $(current_index[])"
+    end
+
+    # Ajoute un slider pour naviguer entre les images
+    slider = Slider(fig[3, 1:2], range = 1:length(images), startvalue = 1)
+    on(slider.value) do val
+        current_index[] = val
+        @info "Slider déplacé - Index: $val"
+    end
+
+    # Affichage de la fenêtre
+    display(fig)
 end
-display(app) # display it in browser or plotpane
-export_static("app.html", app) # generated self contained HTML file from App
-export_static("folder", Routes("/" => app)) # Export static site (without Julia connection)
-# Or serve it on a server
-server = Server(app, "127.0.0.1", 8888)
-# add it as different route
-# regex, and even custom matchers are possible for routes, read more in the docs!
-route!(server, "/my-route" => app)
+
+# Lance l'interface
+interface_images()
